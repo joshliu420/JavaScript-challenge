472d646d8f50cf354ca6064b36b087ef65040a9c
joshliu420 <58458749+joshliu420@users.noreply.github.com>
2020-04-19 00:29:22 -0700

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//Import all the rows from data
 var ttable = d3.select("tbody");
 
 tableData.forEach((report) => {
@@ -13,30 +13,72 @@ tableData.forEach((report) => {
 });
 
 
-// var selectdropdown = d3.select("#selDataset");
-// selectdropdown.on("click", function(listvalue) {
-//   var value = listvalue.value;
+//When select from dropdown menu, the display text change accordingly 
+var selectdropdown = d3.select("#selDataset");
+selectdropdown.on("change", function() {
+  var value = selectdropdown.property("value");
+  console.log(value)
 
-//   if(value === "City") {
-//     textlabel.text("Enter a City");
-//   };
+  if(value === "date") {
+    d3.select("#textlabel").text(t => "Enter a Date");
+    d3.select(".form-control").attr("placeholder", "1/1/2010");
+  }
 
-// });
+  else if(value === "city") {
+    d3.select("#textlabel").text(t => "Enter a City");
+    d3.select(".form-control").attr("placeholder", "ackerman, windham, etc.");
+  }
+
+  else if(value === "state") {
+    d3.select("#textlabel").text(t => "Enter a State");
+    d3.select(".form-control").attr("placeholder", "ar, wa, etc.");
+  }
+
+  else if(value === "country") {
+    d3.select("#textlabel").text(t => "Enter a Country");
+    d3.select(".form-control").attr("placeholder", "us / ca");
+  }
+
+  else if(value === "shape") {
+    d3.select("#textlabel").text(t => "Enter a Shape");
+    d3.select(".form-control").attr("placeholder", "circle, fireball, etc.");
+  };
+
+});
+
 
 function search() {
 
   //Remove the current table
-  // $("tbody tr").remove();
+  d3.select("tbody").selectAll("tr").remove();
 
-  //Read the value in the input 
-  var selectvalue = d3.select("#datetime");
-  var inputvalue = selectvalue.node().value;
+  //Read the input value 
+  var inputvalue = d3.select("#datetime").node().value.toLowerCase();
+
+  //Read the selection from dropdown menu 
+  var value = selectdropdown.property("value");
 
   //Find the match from data.js
-  var filteredData = tableData.filter(input => input.datetime === inputvalue);
-  console.log(filteredData);
+  if (value === 'date') {
+    var filteredData = tableData.filter(input => input.datetime === inputvalue);
+    console.log(filteredData);}
+    
+  else if (value === 'city') {
+    var filteredData = tableData.filter(input => input.city === inputvalue);
+    console.log(filteredData);}
+    
+  else if (value === 'state') {
+    var filteredData = tableData.filter(input => input.state === inputvalue);
+    console.log(filteredData);}
 
-   
+  else if (value === 'country') {
+    var filteredData = tableData.filter(input => input.country === inputvalue);
+    console.log(filteredData);}
+
+  else if (value === 'shape') {
+    var filteredData = tableData.filter(input => input.shape === inputvalue);
+    console.log(filteredData);};
+  
   //Rewrite the table row for filtered data only
   var ttable = d3.select("tbody");
   filteredData.forEach((report) => {
@@ -51,6 +93,6 @@ function search() {
     
 };
 
+//Apply the search function when click the "Filter Table" button
 var selectbutton = d3.select("#filter-btn");
-
 selectbutton.on("click", search);
